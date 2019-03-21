475d13d0f31167be251238c112fa3e70445ed930
Simon Warta <simon@kullo.net>
2019-03-21 13:03:15 +0100

    Rebuild example projects from local web3x build

--- example-projects/webpack/src/balance-fetcher.test.ts
+++ example-projects/webpack/src/balance-fetcher.test.ts
@@ -1,4 +1,5 @@
 import { getBalance } from './balance-fetcher';
+import { Address } from 'web3x-es/address';
 
 describe('test suite', () => {
   let mockEth: any;
@@ -12,9 +13,9 @@ describe('test suite', () => {
   it('should fetch balance', async () => {
     mockEth.getBalance.mockResolvedValue('1000000000000000000');
 
-    const balance = await getBalance(mockEth, '0x0000000000000000000000000000000000000000');
+    const balance = await getBalance(mockEth, Address.ZERO);
 
     expect(balance).toBe('1');
-    expect(mockEth.getBalance).toHaveBeenCalledWith('0x0000000000000000000000000000000000000000');
+    expect(mockEth.getBalance).toHaveBeenCalledWith(Address.ZERO);
   });
 });
