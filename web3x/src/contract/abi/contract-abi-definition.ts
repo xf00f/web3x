e982effa6ee66080f660d3731a9ee780e74f61ca
megaturbo <megaturbo@truelevel.io>
2019-09-23 11:53:34 +0200

    Add optional field `internalType` in AbiInput and AbiOutput

--- web3x/src/contract/abi/contract-abi-definition.ts
+++ web3x/src/contract/abi/contract-abi-definition.ts
@@ -16,8 +16,19 @@
 */
 
 export type AbiDataTypes = 'uint256' | 'boolean' | 'string' | 'bytes' | string;
-export type AbiInput = { components?: any; name: string; type: AbiDataTypes; indexed?: boolean };
-export type AbiOutput = { components?: any; name: string; type: AbiDataTypes };
+export type AbiInput = {
+  components?: any;
+  name: string;
+  type: AbiDataTypes;
+  indexed?: boolean;
+  internalType?: string;
+};
+export type AbiOutput = {
+  components?: any;
+  name: string;
+  type: AbiDataTypes;
+  internalType?: string;
+};
 
 export interface ContractEntryDefinition {
   constant?: boolean;
